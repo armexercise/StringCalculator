11072a2ed770ace315baab80f6f592cef0f69153
armexercise <paramdeep.s.pahwa+arm@gmail.com>
2015-08-06 11:52:58 +0100

    Add Commons Utils and pass the test

--- src/main/java/co/uk/arm/exercise/StringCalculator.java
+++ src/main/java/co/uk/arm/exercise/StringCalculator.java
@@ -2,8 +2,22 @@ package co.uk.arm.exercise;
 
 /**
  * StringCalculator
- * @author Paramdeep
+ * 
+ * @author PP
+ *
  */
 public class StringCalculator {
+
+	/**
+	 * Returns 0
+	 * 
+	 * @param string
+	 *            numbers
+	 * @return total
+	 */
+	public int add(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }

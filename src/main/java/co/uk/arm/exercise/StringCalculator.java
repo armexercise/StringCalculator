880b464bd018418f46988b8974f82cb916f3851e
armexercise <paramdeep.s.pahwa+arm@gmail.com>
2015-08-06 11:49:41 +0100

    Add StringCalculator

---
+++ src/main/java/co/uk/arm/exercise/StringCalculator.java
@@ -0,0 +1,9 @@
+package co.uk.arm.exercise;
+
+/**
+ * StringCalculator
+ * @author Paramdeep
+ */
+public class StringCalculator {
+
+}
--- src/test/java/co/uk/arm/exercise/StringCalculatorTest.java
+++ src/test/java/co/uk/arm/exercise/StringCalculatorTest.java
@@ -2,13 +2,15 @@ package co.uk.arm.exercise;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.xml.bind.ValidationException;
+
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * String Calculator Test Class
- * 
- * @author Paramdeep
+ *
+ * @author pp
  */
 public class StringCalculatorTest {
 
@@ -20,7 +22,7 @@ public class StringCalculatorTest {
 	}
 
 	@Test
-	public void add_WithEmptyParam_Zero()  {
+	public void add_WithEmptyParam_Zero() throws ValidationException {
 		int result = calculator.add("");
 		assertEquals(0, result);
 	}
